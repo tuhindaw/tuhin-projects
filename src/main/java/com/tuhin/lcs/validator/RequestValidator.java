ac0f46ef46922de5d0cbe8c316ca4aa24a3fe3df
tuhindaw <tuhin.daw@gmail.com>
2020-09-09 02:15:27 -0400

    Just added json format validation

--- src/main/java/com/tuhin/lcs/validator/RequestValidator.java
+++ src/main/java/com/tuhin/lcs/validator/RequestValidator.java
@@ -1,8 +1,12 @@
 package com.tuhin.lcs.validator;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tuhin.lcs.domain.Request;
 import com.tuhin.lcs.exception.BadLCSRequestException;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,13 +21,21 @@ public class RequestValidator {
             throw new BadLCSRequestException("Request body can't be empty");
         }
 
+        //validate - input JSON format
+        validateRequestJSONFormat(request);
+
         //validate - setOfStrings is not empty
         if(CollectionUtils.isEmpty(request.getSetOfStrings())){
             throw new BadLCSRequestException("Input string can't be empty");
         }
+        List<String> inputs = request.getSetOfStrings().stream().map(t->t.value).collect(Collectors.toList());
+        for(String s : inputs){
+            if(StringUtils.isEmpty(s)){
+                throw new BadLCSRequestException("Must provide all the input String values OR input child array JSON property is other then 'value' ");
+            }
+        }
 
         //validate - setOfStrings must be unique
-        List<String> inputs = request.getSetOfStrings().stream().map(t->t.value).collect(Collectors.toList());
         HashMap<String, Integer> hm = new HashMap<>();
         boolean hasDuplicate = false;
         // Store counts of all elements in map hm
@@ -34,14 +46,31 @@ public class RequestValidator {
 
             hm.put(inputs.get(i), hm.get(inputs.get(i))+1);
         }
+        String duplicateValue = "";
         for(String key: hm.keySet()){
             if(hm.get(key)>1){
                 hasDuplicate = true;
+                duplicateValue = key;
+                break;
             }
         }
         if(hasDuplicate){
-            throw new BadLCSRequestException("Input string must be unique");
+            throw new BadLCSRequestException("Input string must be unique but you have provided duplicate string => "+duplicateValue);
         }
 
+    }
+
+    public static void validateRequestJSONFormat(Request request) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(request);
+            JsonNode node = objectMapper.readTree(json);
+            if(!node.hasNonNull("setOfStrings")){
+                throw new BadLCSRequestException("Input JSON format is not correct, json root property is other than 'setOfStrings' ");
+            }
+
+        } catch (JsonProcessingException ex) {
+            throw new BadLCSRequestException("Input JSON format is not correct");
+        }
     }
 }
